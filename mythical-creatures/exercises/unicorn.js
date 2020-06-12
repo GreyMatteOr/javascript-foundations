8d0af2cd8c2dd80854157c60c3a09acb00b44205
Matthew Lane <themplane@gmail.com>
2020-06-12 15:20:20 -0700

    Fix unicorn.js, unskip and pass all unicorn-tests.js

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,11 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
   }
+
+  isWhite(){ return this.color == 'white' }
+  says(str){ return '**;* '+ str +' *;**' }
 }
 
 module.exports = Unicorn;
