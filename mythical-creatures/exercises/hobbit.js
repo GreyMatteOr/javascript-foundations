4849a1cc07fa9c45baf80eaaf555f61d1dfd20eb
Matthew Lane <themplane@gmail.com>
2020-06-13 13:41:58 -0700

    fix hobbit.js, unskip and pass all hobbit-test.js

---
+++ mythical-creatures/exercises/hobbit.js
@@ -0,0 +1,19 @@
+class Hobbit{
+  constructor(name, age = 0, adult = ( age >= 33 ), old = (age >= 101), hasRing = (name == 'Frodo')){
+    this.name = name
+    this.age = age
+    this.adult = adult
+    this.old = old
+    this.hasRing = hasRing
+  }
+  celebrateBirthday(){
+    this.age++
+    if( this.age >= 33 ){
+      this.adult = true
+    }
+    if( this.age >= 101){
+      this.old = true
+    }
+  }
+}
+module.exports = Hobbit
